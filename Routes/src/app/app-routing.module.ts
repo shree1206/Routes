023bcbe0658c5f4eb91877ed427967a22331a238
Vaibhav Chaudhary <vchaudhary3@worldbankgroup.org>
2023-12-18 17:17:17 +0530

    Introduction to Angular Routing. Set up and load routes with RouterModule in angular

--- Routes/src/app/app-routing.module.ts
+++ Routes/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { CategoryComponent } from './category/category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UserComponent },
+  { path: 'category', component: CategoryComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Routes/src/app/app.module.ts
+++ Routes/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
