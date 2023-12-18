b7d3911e6af0c8f1c711fa0bf2625a29d0ba7b5c
Vaibhav Chaudhary <vchaudhary3@worldbankgroup.org>
2023-12-18 18:44:28 +0530

    Navigate between pages using navigate and navigateByUrl

--- Routes/src/app/user/user.component.ts
+++ Routes/src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class UserComponent {
 
+  constructor(private _route: Router) { }
+  changeRoute() {
+    //this._route.navigateByUrl('/category');
+    this._route.navigate(['/category']);
+  }
 }
